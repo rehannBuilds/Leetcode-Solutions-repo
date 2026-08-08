c28f8f1ab5805ed4f993b9cc592fb0297f8e2041
Rehan Siddiqui <studyrehan1@gmail.com>
2026-08-08 14:23:53 +0530

    Add files via upload

---
+++ maxConsOne.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ptrr=0;
+        int count=0;
+        int maxi;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count++;
+            }
+            maxi=max(maxi,count);
+            if(nums[i]==0){
+                count=0;
+            }
+            
+        }
+        return maxi;
+    }
+};
